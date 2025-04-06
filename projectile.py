48b6d16af3603265f383718e2d84a176ab65b0f1
Nacho828 <nchomaxanton@gmail.com>
2025-04-06 17:15:46 +0200

    gota

--- projectile.py
+++ projectile.py
@@ -4,7 +4,7 @@ class Projectile:
     def __init__(self, x, y):
         # Cargar la imagen del proyectil
         self.image = pygame.image.load("assets/projectile.png")  # Ruta del sprite del disparo
-        self.image = pygame.transform.scale(self.image, (20, 40))  # Escalar la imagen si es necesario
+        self.image = pygame.transform.scale(self.image, (50, 50))  # Escalar la imagen si es necesario
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.speed = -10  # Velocidad hacia arriba
