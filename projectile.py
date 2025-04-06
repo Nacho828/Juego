568ece4e9fe824616c43408ae03f6ea0d6a4ccd0
Nacho828 <nchomaxanton@gmail.com>
2025-04-06 19:16:41 +0200

    j

--- projectile.py
+++ projectile.py
@@ -1,16 +1,16 @@
 import pygame
 
 class Projectile:
-    def __init__(self, x, y, color, speed=-10):
+    def __init__(self, x, y, color, speed=5):  # Velocidad positiva para proyectiles enemigos
         self.image = pygame.Surface((10, 20))  # Tamaño del proyectil
         self.image.fill(color)  # Color del proyectil
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.speed = speed  # Velocidad del proyectil (hacia arriba)
+        self.speed = speed  # Velocidad del proyectil
 
     def move(self):
-        """Mover el proyectil hacia arriba."""
-        self.rect.y += self.speed
+        """Mover el proyectil."""
+        self.rect.y += self.speed  # Mover hacia abajo si la velocidad es positiva
 
     def draw(self, screen):
         """Dibujar el proyectil en la pantalla."""
