9e45e3f28b326709c54b081000a67e207769a65f
Nacho828 <nchomaxanton@gmail.com>
2025-04-06 20:59:05 +0200

    f

--- main1.py
+++ main1.py
@@ -1,22 +1,7 @@
 import pygame
 from Game1 import Game
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Juego Arcade")
-        self.clock = pygame.time.Clock()
-        self.is_running = False
-        self.score = 0
-        self.lives = 3  # Vida inicial del jugador
-        self.player = Player(375, 500)
-        self.opponents = [
-            Opponent(100, 50, "assets/enemy1.png"),
-            Opponent(300, 100, "assets/enemy2.png"),
-            Opponent(500, 150, "assets/enemy3.png")
-        ]
-        self.projectiles = []  # Lista para almacenar los proyectiles del jugador
+
 
 def main():
     print("¡Bienvenido al juego!")
