020d24a631efd186b73779887ca5d19f0f31738c
Nacho828 <nchomaxanton@gmail.com>
2025-04-06 21:11:16 +0200

    proyectiled

--- projectile.py
+++ projectile.py
@@ -1,9 +1,10 @@
 import pygame
 
 class PlayerProjectile:
-    def __init__(self, x, y, color=(255, 255, 0), speed=-10):
-        self.image = pygame.Surface((10, 20))  # Tamaño del proyectil
-        self.image.fill(color)  # Color del proyectil
+    def __init__(self, x, y, sprite_path="assets/player_projectile.png", speed=-10):
+        # Cargar la imagen del proyectil del jugador
+        self.image = pygame.image.load(sprite_path)
+        self.image = pygame.transform.scale(self.image, (15, 30))  # Escalar la imagen si es necesario
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.speed = speed  # Velocidad del proyectil (hacia arriba)
@@ -18,9 +19,10 @@ class PlayerProjectile:
 
 
 class EnemyProjectile:
-    def __init__(self, x, y, color=(255, 0, 0), speed=5):
-        self.image = pygame.Surface((10, 20))  # Tamaño del proyectil
-        self.image.fill(color)  # Color del proyectil
+    def __init__(self, x, y, sprite_path="assets/enemy_projectile.png", speed=5):
+        # Cargar la imagen del proyectil del enemigo
+        self.image = pygame.image.load(sprite_path)
+        self.image = pygame.transform.scale(self.image, (15, 30))  # Escalar la imagen si es necesario
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.speed = speed  # Velocidad del proyectil (hacia abajo)
